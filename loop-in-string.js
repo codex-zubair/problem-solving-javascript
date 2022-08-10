de39a304d58e6c632899a24a70e32e1ff8a5ee52
Rocko <codingwithrock@gmail.com>
2022-08-10 19:40:10 +0600

    first commit

---
+++ loop-in-string.js
@@ -0,0 +1,6 @@
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/codingwithrock/problem-solving-javascript.git
+git push -u origin mainJ
