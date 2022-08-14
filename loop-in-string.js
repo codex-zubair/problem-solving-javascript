341d1bceb60440279a92e086eb29d379134830cf
Joya322 <jannatferdoush1212@gmail.com>
2022-08-14 22:57:52 +0600

    opening of new branch

--- loop-in-string.js
+++ loop-in-string.js
@@ -3,10 +3,18 @@ const love = "amar sonar javascript ami tumae valo bashi"
 
 // *In for index value
 for (i in love) {
-    console.log(i);
+    // console.log(i);
 }
 
 // * of for direct value
 for (j of love) {
-    console.log(j);
+    // console.log(j);
 }
+
+const studentOfJhankarVai =
+  ".ahahah .ekaht eyin evol ilahk .ahtok elob tneduts re iav raknahJ .ahceg ahtam re reyabuz";
+
+function reverse(student) {
+    return student.split("").reverse().join("");
+}
+console.log(reverse(studentOfJhankarVai));
