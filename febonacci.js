da575823bce7cafb54351ae0fa75c47fc07df2be
Rocko <codingwithrock@gmail.com>
2022-09-06 23:03:46 +0600

    febonacci series added

---
+++ febonacci.js
@@ -0,0 +1,17 @@
+// let fibo = [15, 30]
+// for (var i = 2; i <= 10; i++) {
+//     fibo[i] = fibo[i - 1] + fibo[i - 2];
+// }
+// console.log(fibo)
+
+
+
+var fibonacci = [15,30];
+var x = 10;
+
+for(i = 2; i<=x; i++)
+
+{
+    fibonacci[i] = fibonacci[i-1] + fibonacci[i-2]; 
+}
+console.log(fibonacci);
